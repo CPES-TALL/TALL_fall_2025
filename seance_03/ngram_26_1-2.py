2fd8db11853d0e63d4f09b415698d3fb6496d0cd
Pascal Amsili <Pascal.Amsili@ens.fr>
2025-09-18 14:41:32 +0200

    corrigé question 2

---
+++ seance_03/ngram_26_1-2.py
@@ -0,0 +1,75 @@
+# ---------------------------------------- -*- coding: utf-8 -*-
+# N.B. Dans le dictionnaire le contexte est un tuple (même pour n=2)
+
+import nltk
+
+# Si nécessaire, télécharger punkt_tab
+#nltk.download('punkt_tab')
+
+def charge_corpus(filename):
+    ''' retourne une liste de phrases
+    qui sont elles-même des listes de tokens'''
+    corpus = []
+    with open(filename, "r") as f:
+        for p in nltk.tokenize.sent_tokenize(f.read()):
+            corpus.append(nltk.word_tokenize(p))
+    return corpus
+
+def construit_modele(corpus, n=2):
+    modele = {}
+    for phrase in corpus:
+        phrase = ['<s>'] + phrase + ['</s>']
+        for i in range(len(phrase) - n + 1):
+            contexte, mot = tuple(phrase[i:i+n-1]), phrase[i+n-1]
+            if contexte in modele.keys():
+                if mot in modele[contexte].keys():
+                    modele[contexte][mot] += 1
+                else:
+                    modele[contexte][mot] = 1
+            else:
+                modele[contexte] = { mot : 1 }
+    return modele
+
+def prediction_greedy(modele, n, contexte):
+    '''prédit le mot qui doit suivre en prenant
+    les derniers n-1 mots du contexte (si possible)'''
+    if len(contexte) >= n-1:
+        cle = tuple(contexte[-(n-1):])
+        if cle in modele.keys():
+            choix = modele[cle]
+            return max(choix, key=choix.get)
+    return 'XXXX'
+
+
+def verification_prediction(m,N):
+    for w in ['<s>', 'Le', 'Candide', 'petite']:
+        contexte = (w,)
+        print("Après le mot %s le modèle prédit %s." % (w,prediction_greedy(modele,N,contexte)))
+
+
+def verification_modele(m):
+    cle = ('<s>',)
+    print(modele[cle])
+    cle = ('petite',)
+    print(modele[cle])
+    
+
+# VARIABLES GLOBALES & PARAMÈTRES
+
+# Corpus de travail
+corpus_name = "Candide.txt"
+
+# Taille des n-grammes
+N = 2
+
+
+# PROGRAMME PRINCIPAL
+if __name__ == '__main__':
+    print("Chargement du corpus... ", end='')
+    corpus = charge_corpus(corpus_name)
+    print("%d tokens chargés. " % len(corpus))
+    modele = construit_modele(corpus, N)
+    print("Modèle n-gramme calculé, n=%d" % N)
+
+#    verification_modele(modele)
+    verification_prediction(modele,N)
